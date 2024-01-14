6b2703b9c04da33d7a06b55526066fbe79b32d7e
Fábio Maiorano <fsmaiorano@gmail.com>
2024-01-14 20:09:45 +0000

    Update app and home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,14 @@ import { MaterialModule } from '../shared/material/material.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  name = 'Fábio Santini Maiorano';
+  nationality = 'Brazilian';
+  email = 'fsmaiorano@gmail.com';
+  phone = '+351 910 069 584';
+  linkedin = 'https://www.linkedin.com/in/fsmaiorano';
+  address = 'Lisbon, Portugal';
+  photo = 'https://github.com/fsmaiorano.png';
+  aboutMe =
+    "I'm a software engineer with 10+ years of experience in the field. I have a bachelor's degree in Computer Science and a master's degree in Software Engineering. I'm passionate about technology and I'm always looking for new challenges and opportunities to learn and grow. I'm currently living in Lisbon, Portugal, and I'm looking for a new job opportunity in the area.";
+}
